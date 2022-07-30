6569af81df33b37e3e9cb510a9ab7b4dbe3f6b95
Ethan Warren <ethan.warren10@gmail.com>
2022-07-30 11:36:37 +0100

    Fix className error on line 5 of Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,7 +2,7 @@ const Gallery = ({ gallery }) => {
   return (
     <div className="gallery-area">
       {gallery.map(img => (
-        <div class="gallery-item" key={img.id}>
+        <div className="gallery-item" key={img.id}>
           <img src={img.url} alt={img.alt} />
         </div>
       ))}
